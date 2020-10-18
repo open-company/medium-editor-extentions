c34dfd5a31af537c287cc708621a579c1508f404
bago2k4 <iacopo@rc0.eu>
2020-10-18 18:13:39 +0200

    Fix relative paths.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-// export {Toolbar} from "./src/Toolbar/index";
-export {default as PasteHandler} from "./src/Paste/index";
-export {default as Autolist} from "./src/Autolist/index";
-export {default as AutoQuote} from "./src/Autoquote/index";
-export {default as AutoCode} from "./src/Autocode/index";
-export {default as AutoInlinecode} from "./src/Autoinlinecode/index";
-export {default as InlineCodeButton} from "./src/Inlidecodebutton/index";
-export {default as HighlighterButton} from "./src/Highlighterbutton/index";
+// export {Toolbar} from "src/Toolbar/index";
+export {default as PasteHandler} from "src/Paste/index";
+export {default as Autolist} from "src/Autolist/index";
+export {default as AutoQuote} from "src/Autoquote/index";
+export {default as AutoCode} from "src/Autocode/index";
+export {default as AutoInlinecode} from "src/Autoinlinecode/index";
+export {default as InlineCodeButton} from "src/Inlidecodebutton/index";
+export {default as HighlighterButton} from "src/Highlighterbutton/index";
