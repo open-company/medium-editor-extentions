f2f6d3f2efe326e497b08696f662342f45c17feb
bago2k4 <iacopo@rc0.eu>
2020-10-18 22:25:24 +0200

    Typo

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ export {default as AutoInlineCode} from "./src/AutoInlineCode/index.js";
 export {default as AutoList} from "./src/AutoList/index.js";
 export {default as AutoQuote} from "./src/AutoQuote/index.js";
 export {default as HighlighterButton} from "./src/HighlighterButton/index.js";
-export {default as InlineCodeButton} from "./src/InlideCodeButton/index.js";
+export {default as InlineCodeButton} from "./src/InlineCodeButton/index.js";
 export {default as PasteHandler} from "./src/PasteHandler/index.js";
