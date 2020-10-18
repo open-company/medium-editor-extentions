819d29da4d3f5c6bc4c993e02d142cfa4aca68c9
bago2k4 <iacopo@rc0.eu>
2020-10-18 17:25:04 +0200

    Add all extentions in src subfolders, export all needed modules in index.js.

---
+++ index.js
@@ -0,0 +1,10 @@
+
+export {Toolbar} from "./src/Toolbar/index";
+export {PasteHandler} from "./src/Paste/index";
+export {PasteHandler} from "./src/Paste/index";
+export {Autolist} from "./src/Autolist/index";
+export {AutoQuote} from "./src/Autoquote/index";
+export {AutoCode} from "./src/Autocode/index";
+export {AutoInlinecode} from "./src/Autoinlinecode/index";
+export {InlineCodeButton} from "./src/Inlidecodebutton/index";
+export {HighlighterButton} from "./src/Highlighterbutton/index";
